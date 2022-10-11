4c771d5d8651be29b26ddd30aac61138dc1cafe0
PRAKASH DAKUA <38912537+prakashdakua@users.noreply.github.com>
2022-10-11 19:44:44 +0200

    Update guestbook.js
    change in line 4 10 19

--- guestbook.js
+++ guestbook.js
@@ -7,7 +7,7 @@ const guestbook = {
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${/read-guestbook-entries-sequence.json}/entries`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${/save-guestbook-entry-sequence.json}/entries`,
+      url: `${apiUrl}/save-guestbook-entry-sequence.json`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
